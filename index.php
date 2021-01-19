e37533bfca707e3d54372e2a07268b509ca25086
Andrea <grassiandrea95@gmail.com>
2021-01-19 15:35:47 +0100

    modifica codice

--- index.php
+++ index.php
@@ -3,142 +3,196 @@
   <head>
     <meta charset="utf-8">
     <title></title>
-
-    <style>
-      .container{
-        padding-left: 20px;
-      }
-      .title{
-        margin-bottom: 10px;
-      }
-    </style>
-
   </head>
   <body>
     <div class="container">
       <?php
 
-        class Person{    //persona a livello generale
+        class Person {
           private $name;
-          private $lastName;
-          private $age;
-
-          function __construct($name,$lastName,$age){
-            $this-> setName($name);
-            $this-> setlastName($lastName);
-            $this-> setAge($age);
+          private $lastname;
+          private $dateOfBirth;
+          private $securyLvl;
+          public function __construct($name, $lastname, $dateOfBirth, $securyLvl) {
+            $this -> setName($name);
+            $this -> setLastname($lastname);
+            $this -> setDateOfBirth($dateOfBirth);
+            $this -> setSecuryLvl($securyLvl);
           }
-
           public function getName() {
             return $this -> name;
           }
           public function setName($name) {
             $this -> name = $name;
           }
-
-          public function getlastName() {
-            return $this -> lastName;
+          public function getLastname() {
+            return $this -> lastname;
           }
-          public function setlastName($lastName) {
-            $this -> lastName = $lastName;
+          public function setLastname($lastname) {
+            $this -> lastname = $lastname;
           }
-
-          public function getAge() {
-            return $this -> age;
+          public function getFullname() {
+            return $this -> getName() . ' ' . $this -> getLastname();
           }
-          public function setAge($age) {
-            $this -> age = $age;
+          public function getDateOfBirth() {
+            return $this -> dateOfBirth;
           }
-
-          public function __toString() {    //traduce l'oggetto in forma letterale
-          return
-            'Name: ' . $this -> getName() . '<br>'
-            . 'LastName: ' . $this -> getlastName() . '<br>'
-            . 'Age: ' . $this -> getAge();
+          public function setDateOfBirth($dateOfBirth) {
+            $this -> dateOfBirth = $dateOfBirth;
           }
-
+          public function getSecuryLvl() {
+            return $this -> securyLvl;
+          }
+          public function setSecuryLvl($securyLvl) {
+            $this -> securyLvl = $securyLvl;
+          }
+          public function __toString() {
+            return
+              'name: ' . $this -> getName() . '<br>'
+              . 'lastname: ' . $this -> getLastname() . '<br>'
+              . 'dateOfBirth: ' . $this -> getDateOfBirth() . '<br>'
+              . 'securyLvl: ' . $this -> getSecuryLvl() . '<br>';
+          }
         }
-
-        class Employee extends Person{   //dipendente
-
-          private $role;   //ruolo del dipendente
-
-          function __construct($name, $lastName, $age, $role){
-            parent::__construct($name, $lastName, $age);
-            $this-> setRole($role);
+        // VERSIONE 1
+        class Employee extends Person {
+          private $ral;
+          private $mainTask;
+          private $idCode;
+          private $dateOfHiring;
+          public function __construct($name, $lastname, $dateOfBirth, $securyLvl, $ral, $mainTask, $idCode, $dateOfHiring) {
+            parent::__construct($name, $lastname, $dateOfBirth, $securyLvl);
+            $this -> setRal($ral);
+            $this -> setMainTask($mainTask);
+            $this -> setIdCode($idCode);
+            $this -> setDateOfHiring($dateOfHiring);
           }
-
-          public function getRole() {
-            return $this -> role;
+          public function getRal() {
+            return $this -> $ral;
           }
-          public function setRole($role) {
-            $this -> role = $role;
+          public function setRal($ral) {
+            $this -> ral = $ral;
           }
-
+          public function getMainTask() {
+            return $this -> $mainTask;
+          }
+          public function setMainTask($mainTask) {
+            $this -> mainTask = $mainTask;
+          }
+          public function getIdCode() {
+            return $this -> $idCode;
+          }
+          public function setIdCode($idCode) {
+            $this -> idCode = $idCode;
+          }
+          public function getDateOfHiring() {
+            return $this -> $dateOfHiring;
+          }
+          public function setDateOfHiring($dateOfHiring) {
+            $this -> dateOfHiring = $dateOfHiring;
+          }
           public function __toString() {
             return parent::__toString() . '<br>'
-              . 'Role: ' . $this -> getRole();
-            }
+              . 'ral: ' . $this -> ral . '<br>'
+              . 'mainTask: ' . $this -> mainTask . '<br>'
+              . 'idCode: ' . $this -> idCode . '<br>'
+              . 'dateOfHiring: ' . $this -> dateOfHiring . '<br>';
+          }
         }
 
-        class Boss extends Person{    //boss
-
-          private $roleBoss;   //ruolo del boss
-          private $nameCompany;   //nome dell'azienda
-
-          function __construct($name, $lastName, $age, $roleBoss, $nameCompany){
-            parent::__construct($name, $lastName, $age);
-            $this-> setRoleBoss($roleBoss);
-            $this-> setnameCompany($nameCompany);
+        class Boss extends Employee {
+          private $profit;
+          private $vacancy;
+          private $sector;
+          private $employees;
+          public function __construct($name, $lastname, $dateOfBirth, $securyLvl, $ral, $mainTask, $idCode, $dateOfHiring, $profit, $vacancy, $sector, $employees = []) {
+            parent::__construct($name, $lastname, $dateOfBirth, $securyLvl, $ral, $mainTask, $idCode, $dateOfHiring);
+            $this -> setProfit($profit);
+            $this -> setVacancy($vacancy);
+            $this -> setSector($sector);
+            $this -> setEmployees($employees);
           }
-
-          public function getRoleBoss() {
-            return $this -> roleBoss;
+          public function getProfit() {
+            return $this -> profit;
           }
-          public function setRoleBoss($roleBoss) {
-            $this -> roleBoss = $roleBoss;
+          public function setProfit($profit) {
+            $this -> profit = $profit;
           }
-
-          public function getNameCompany() {
-            return $this -> nameCompany;
+          public function getVacancy() {
+            return $this -> vacancy;
           }
-          public function setNameCompany($nameCompany) {
-            $this -> nameCompany = $nameCompany;
+          public function setVacancy($vacancy) {
+            $this -> vacancy = $vacancy;
           }
-
+          public function getSector() {
+            return $this -> sector;
+          }
+          public function setSector($sector) {
+            $this -> sector = $sector;
+          }
+          public function getEmployees() {
+            return $this -> employees;
+          }
+          public function setEmployees($employees) {
+            $this -> employees = $employees;
+          }
           public function __toString() {
             return parent::__toString() . '<br>'
-              . 'Role: ' . $this -> getRoleBoss() . '<br>'
-              . 'Company: ' . $this-> getNameCompany();
+              . 'profit: ' . $this -> getProfit() . '<br>'
+              . 'vacancy: ' . $this -> getVacancy() . '<br>'
+              . 'sector: ' . $this -> getSector() . '<br>'
+              . 'employees:<br>' . $this -> getEmpsStr() . '<br>';
+          }
+          private function getEmpsStr() {
+            $str = '';
+            for ($x=0;$x<count($this -> getEmployees());$x++) {
+              $emp = $this -> getEmployees()[$x];
+              $fullname = $emp -> getName() . ' ' . $emp -> getLastname();
+              $str .= ($x + 1) . ': ' . $fullname . '<br>';
             }
+            return $str;
+          }
         }
 
-
-        //istanze
-        $boss = new Boss('Andrea', 'lastname', 'age', 'roleBoss', 'nameCompany');
-        $employee1 = new Employee('name', 'lastname', 'age', 'role');
-        $employee2 = new Employee('name', 'lastname', 'age', 'role');
-
-
-        //stampo tutto
-        ?>
-          <h1 class="title">Boss:</h1>
-          <div class="lista">
-            <?php echo $boss ?>
-          </div>
-
-          <h3 class="title">Employee1:</h3>
-          <div class="lista">
-            <?php echo $employee1 ?>
-          </div>
-
-          <h3 class="title">Employee2:</h3>
-          <div class="lista">
-            <?php echo $employee2 ?>
-          </div>
-
-        <?php
-
+        
+        $p1 = new Person(
+          '(p)name',
+          '(p)lastname',
+          '(p)dateOfBirth',
+          '(p)securyLvl',
+        );
+        // echo 'p1:<br>' . $p1 . '<br><br>';
+        $e1 = new Employee(
+          '(e)name',
+          '(e)lastname',
+          '(e)dateOfBirth',
+          '(e)securyLvl',
+          '(e)ral',
+          '(e)mainTask',
+          '(e)idCode',
+          '(e)dateOfHiring',
+        );
+        // echo 'e1:<br>' . $e1 . '<br><br>';
+        $b1 = new Boss(
+          '(b)name',
+          '(b)lastname',
+          '(b)dateOfBirth',
+          '(b)securyLvl',
+          '(b)ral',
+          '(b)mainTask',
+          '(b)idCode',
+          '(b)dateOfHiring',
+          '(b)profit',
+          '(b)vacancy',
+          '(b)sector',
+          [
+              $e1,
+              $e1,
+              $e1,
+              $e1,
+          ]
+        );
+        echo 'b1:<br>' . $b1 . '<br><br>';
       ?>
     </div>
   </body>
